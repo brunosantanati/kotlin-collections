63cc8c84d397dfd7f754136d23bf441b6c620666
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-15 15:09:24 -0300

    Comportamentos do Set: union, subtract e intersect.

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -10,4 +10,19 @@ fun main() {
     assistiramAmbos.add("Ana")
     assistiramAmbos.add("Ana") //não adiciona pois já existe
     println(assistiramAmbos)
+
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid.union(assistiramCursoKotlin))
+    println(assistiramCursoAndroid union assistiramCursoKotlin) //infix
+
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex") //insere duplicidade na lista
+    println(assistiramList)
+    println(assistiramList.toSet()) //remove duplicidade
 }
