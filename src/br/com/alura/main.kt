2d7b21926ecd032f50a66e5c7082a8c22b025b32
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-15 14:19:25 -0300

    Usando distinct() para remover duplicidade.

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,5 +1,11 @@
 package br.com.alura
 
 fun main() {
-    
+    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    /*val assistiramAmbos = mutableListOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)*/
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
+    println(assistiramAmbos.distinct())
 }
