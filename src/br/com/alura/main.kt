bc60a0dc8c9132fb5ec57e059e2b3a201b442c33
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-15 12:54:27 -0300

    Usando encapsulamento no codigo que simula BD.

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,10 +2,16 @@ package br.com.alura
 
 fun main() {
     val banco = BancoDeNomes()
-    banco.nomes.add("Bruno")
+    //banco.nomes.add("Bruno")
+    banco.salva("Bruno")
     println(banco.nomes)
 }
 
 class BancoDeNomes{
-    val nomes = mutableListOf<String>()
+    val dados = mutableListOf<String>()
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String){
+        dados.add(nome)
+    }
 }
