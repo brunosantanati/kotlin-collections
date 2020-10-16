4cd0fd69e6f9429072cec38d7ef8365c8ce01fdf
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-16 00:00:18 -0300

    Exemplo do uso do Map.

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,5 +1,23 @@
 package br.com.alura
 
 fun main() {
-    testaSet()
+    //mapOf<Int, Double>(Pair(1, 20.0)) //podemos deixar explícito que é um map de Int e Double
+    val pedidos = mapOf<Int, Double>(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+
+    println(pedidos)
+
+    val pedido0: Double? = pedidos[0]
+    pedido0?.let {
+        println("pedido 0 - $it")
+    }
+
+    val pedido1: Double? = pedidos[1]
+    pedido1?.let {
+        println("pedido 1 - $it")
+    }
+
+    for(pedido: Map.Entry<Int, Double> in pedidos){
+        println("número do pedido: ${pedido.key}")
+        println("valor do pedido: ${pedido.value}")
+    }
 }
